cd32ac1be1ec93d95a6cf1357c965a964315a199
Mateus Sousa <mateusmaciel340@gmail.com>
2022-03-25 15:06:52 -0300

    Finalizando v1 do projeto

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,8 +1,10 @@
 import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
     body{
         padding: 0; margin: 0; box-sizing: border-box;
+        font-family: "Poppins", sans-serif;
     }
 `
 
