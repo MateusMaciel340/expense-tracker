02671376eb762010b099d60e93575f9c5c926a9a
Mateus Sousa <mateusmaciel340@gmail.com>
2022-03-24 23:33:57 -0300

    Configurando lógica para utilização das categorias / estilo

--- src/components/TabelaItem/TabelaItem.tsx
+++ src/components/TabelaItem/TabelaItem.tsx
@@ -1,6 +1,7 @@
 import * as C from "./style";
 import { Item } from "../../types/Item";
 import { formatarData } from "../../helpers/filtro-data";
+import { categorias } from "../../data/categorias";
 
 type Props = {
     item: Item,
@@ -10,9 +11,17 @@ export const TabelaItem = ({ item }: Props) => {
     return(
         <C.TabelaLinha>
             <C.TabelaColuna>{formatarData(item.data)}</C.TabelaColuna>
-            <C.TabelaColuna>{item.categoria}</C.TabelaColuna>
+            <C.TabelaColuna>
+                <C.Categoria cor={categorias[item.categoria].cor}>
+                    {categorias[item.categoria].titulo}
+                </C.Categoria>
+            </C.TabelaColuna>
             <C.TabelaColuna>{item.titulo}</C.TabelaColuna>
-            <C.TabelaColuna>R$ {item.valor}</C.TabelaColuna>
+            <C.TabelaColuna>
+                <C.Valor cor={categorias[item.categoria].despesa ? "red" : "green"}>
+                    R$ {item.valor}
+                </C.Valor>
+            </C.TabelaColuna>
         </C.TabelaLinha>
     );
 }
--- src/components/TabelaItem/style.ts
+++ src/components/TabelaItem/style.ts
@@ -4,3 +4,10 @@ export const TabelaLinha = styled.tr``;
 export const TabelaColuna = styled.td`
     padding: 10px 0;
 `
+export const Categoria = styled.div<{ cor: string }>`
+    display: inline-block; padding: 5px 10px; border-radius: 5px;
+    color: #FFF; background: ${props => props.cor};
+`
+export const Valor = styled.div<{ cor: string }>`
+    color: ${props => props.cor}
+`
